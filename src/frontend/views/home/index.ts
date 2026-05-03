82a67f88714a4bef04d2864c445a9f4acb26bd29
L2NE-dev <conway24b@gmail.com>
2026-05-03 14:39:14 +0700

    Upd

--- src/frontend/views/home/index.ts
+++
@@ -1,4 +0,0 @@
-export { SpeedDial, makeWallpaper, createCtxMenu } from "./SpeedDial";
-export { initializeOrientedDesktop } from "./OrientDesktop";
-export { bindInteraction, reflectCell, resolveGridCellFromClientPoint } from "./Interact";
-export { openShortcutEditor } from "./ShortcutEditor";
